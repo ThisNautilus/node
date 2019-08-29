15ea3d1f0dd3ab8dc46ace737537bfc7b8a4396e
ThisNautilus <zziaocean@foxmail.com>
2019-08-29 22:43:20 +0800

    finish

---
+++ AMD/alert.js
@@ -0,0 +1,8 @@
+define(function() {
+    var logName = function(str) {
+        return "I am " + str;
+    }
+    return {
+        logName: logName
+    };
+});
--- main.js
+++ main.js
@@ -1,4 +1,11 @@
+/*  commonJS使用
 const common = require('./commonjs/a.js')
 
 console.log(common.str);
 console.log(common.logStr('Dylan'));
+*/
+
+/* AMD使用 */
+require(['AMD/alert'], function(msg) {
+    console.log(msg.logName('Dylan'));
+});
